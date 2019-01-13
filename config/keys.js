859c0b8844b37239585436a2c0be4893d7cb6b51
Ishan Bhargava <ishan13bhargava@gmail.com>
2019-01-14 00:19:26 +0530

    Changes made

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://ishan:pass1234@ds255924.mlab.com:55924/devconnector-ishan"
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+// To connect our database
+
+app.get("/", (req, res) => res.send("<h1>Hello</h1>"));
+//                                   What to show
+const port = process.env.PORT || 5000;
+// Port to run
+
+app.listen(port, () => console.log("Server running"));
